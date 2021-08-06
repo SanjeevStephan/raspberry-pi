73e3c04dc1b2776e535e4394742c6e007898c044
Sanjeev Stephan <37835927+SanjeevStephan@users.noreply.github.com>
2021-08-06 13:56:32 -0700

    Add files via upload
    this python script displays the pi [temperature] and its [ip address] on the 16x2 i2c lcd screen

---
+++ 104-The-Scripts/myclock.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+
+# Simple clock program. Writes the exact time.
+# Demo program for the I2C 16x2 Display from Ryanteck.uk
+# Created by Matthew Timmons-Brown for The Raspberry Pi Guy YouTube channel
+
+# Import necessary libraries for communication and display use
+import os
+import drivers
+import time
+
+from time import sleep
+from datetime import datetime
+from subprocess import check_output  ## to display system info such as ip address
+
+# Load the driver and set it to "display"
+# If you use something from the driver library use the "display." prefix first
+display = drivers.Lcd()
+#Command to fetch ip address and store under the variable name IP
+IP = check_output(["hostname", "-I"]).split()[0]
+
+# func()  code for reading temperature of the pi begins from here
+def measure_temp():
+	temp = os.popen("vcgencmd measure_temp").readline()
+	return (temp.replace("temp=",""))
+
+# pi_temp.py code ends
+
+try:
+    print("Writing to display")
+   # display.lcd_display_string("No time to waste", 1)  # Write line of text to first line of display
+    while True:
+        # Write just the time to the display
+        # display.lcd_display_string(" " + str(IP), 1)
+        # display.lcd_display_string("Smart Home",1)
+        display.lcd_display_string("Pi-Temp : " + measure_temp(),1)
+   
+	#  display.lcd_display_string(str(datetime.now().time()), 2)
+        display.lcd_display_string("Time : " + str( time.strftime("%H:%M:%S")),2)
+        # Uncomment the following line to loop with 1 sec delay
+        # sleep(1)
+except KeyboardInterrupt:
+    # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
+    print("Cleaning up!")
+    display.lcd_clear()
+
+
